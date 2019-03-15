ce57469742095e31395e222c2721081dbed60b65
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2019-03-15 18:40:56 +0530

    added associations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,6 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :leaves
+  has_one :leave_quotum
 end
