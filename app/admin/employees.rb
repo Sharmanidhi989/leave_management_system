77c115ba09f5055682005ebe0e4395409bd4471a
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2019-03-18 19:32:43 +0530

    permitted param for activeadmin

--- app/admin/employees.rb
+++ app/admin/employees.rb
@@ -2,7 +2,17 @@ ActiveAdmin.register Employee do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+  permit_params :email, :first_name, :last_name, :department, :designation, :gender, :date_of_birth, :salary, :password, :password_confirmation, :verified, :registration_id
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :registration_id
+    column :verified
+    column :created_at
+    actions
+  end
 #
 # or
 #
--- app/admin/leaves.rb
+++ app/admin/leaves.rb
@@ -1,8 +1,7 @@
 ActiveAdmin.register Leave do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
+  permit_params :employee_id, :status, :to, :from, :reason, :category
 #
 # or
 #
@@ -11,5 +10,4 @@ ActiveAdmin.register Leave do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-
 end
