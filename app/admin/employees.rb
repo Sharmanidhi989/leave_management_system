d9006421dee7796a65c77a8aa3fb65d41812952f
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2019-03-20 18:22:07 +0530

    updated employee and leave views for the activeadmin

--- app/admin/employees.rb
+++ app/admin/employees.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Employee do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
+
   permit_params :email, :first_name, :last_name, :department, :designation, :gender, :date_of_birth, :salary, :password, :password_confirmation, :registration_id
 
   index do
@@ -13,13 +13,40 @@ ActiveAdmin.register Employee do
     column :created_at
     actions
   end
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
+  form title: 'Employee' do |f|
+    inputs 'Details' do
+      input :email
+      input :first_name
+      input :last_name
+      input :department
+      input :designation
+      input :gender
+      input :date_of_birth
+      input :salary
+      input :password
+      input :password_confirmation
+    end
+    actions
+  end
+  
+  action_item do
+    link_to 'Invite New Employee', new_invitation_admin_employees_path
+  end
+
+  collection_action :new_invitation do
+    @employee = Employee.new
+  end 
+  
+  collection_action :send_invitation, :method => :post do
+    @employee = Employee.invite!(params[:employee], current_employee)
+    if @employee.errors.empty?
+      flash[:success] = "employee has been successfully invited." 
+      redirect_to admin_employees_path
+    else
+      messages = @employee.errors.full_messages.map { |msg| msg }.join
+      flash[:error] = "Error: " + messages
+      redirect_to new_invitation_admin_employees_path
+    end
+  end
 end
--- app/admin/leaves.rb
+++ app/admin/leaves.rb
@@ -1,13 +1,12 @@
 ActiveAdmin.register Leave do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  permit_params :employee_id, :status, :to, :from, :category
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  permit_params :employee_id, :status, :to, :from, :leave_quotum_id
+
+  form title: 'Leave' do |f|
+    inputs 'Details' do
+      input :status
+    end
+    actions
+  end
 end
