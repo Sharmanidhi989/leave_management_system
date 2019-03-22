f85c62c7af07217fb0646d26e86783b2eb1d4e47
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2019-03-22 11:50:12 +0530

    fixed migration to suit pg

--- db/migrate/20190318102928_add_default_to_leaves.rb
+++ db/migrate/20190318102928_add_default_to_leaves.rb
@@ -1,5 +1,6 @@
 class AddDefaultToLeaves < ActiveRecord::Migration[5.2]
   def change
-    change_column :leaves, :status, :integer
+    # change_column :leaves, :status, :integer
+    change_column :leaves, :status, 'integer USING CAST(status AS integer)'
   end
 end
