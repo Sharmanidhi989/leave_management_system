89d822079368c632dc8da559f91e4ed420bdd0bd
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2019-04-05 12:26:02 +0530

    removed routes for employee sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :employees
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'welcome/index'
   root 'welcome#index'
   resources :leaves
   resources :leave_quota
+  devise_for :employees, :skip => [:registrations] 
+    as :employee do
+    get 'employees/edit' => 'devise/registrations#edit', :as => 'edit_employee_registration'
+    put 'employees' => 'devise/registrations#update', :as => 'employee_registration'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
